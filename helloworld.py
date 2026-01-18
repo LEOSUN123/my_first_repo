c14c17ad40bef6d089dad98f1ebc3459364ce6e1
leosun <leosunsjtu@163.com>
2026-01-19 00:12:47 +0800

    fullfil more coede

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,8 @@
 print("Hello world!")
+print("Welcome to Python programming.")
+import math
+print("The square root of 16 is:", math.sqrt(16))
+print("The value of pi is approximately:", math.pi)
+print("Goodbye!")
+import pandas as pd
+import selenium
